f1e1ce3a98201d7256267e71fde765c1349f92f7
Nick Fields <nickfields@invisionapp.com>
2019-11-22 13:17:09 -0500

    Initial commit of test repo

---
+++ .eslintrc.pass.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  parserOptions: {
+    sourceType: "module",
+  },
+  rules: {
+    "spaced-comment": "error",
+  },
+  // overrides: [
+  //   {
+  //     files: ["*.ts"],
+  //     extends: [
+  //       "plugin:@typescript-eslint/recommended",
+  //     ],
+  //     rules: {
+  //       "global-require": "off"
+  //     }
+  //   }
+  // ]
+}
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+//this should fail if the rule is disabled
+console.log('hello codeclimate')
